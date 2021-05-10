026aa061d371497ee295f3d22cd50c53f68a4465
rstrietzel <robert.strietzel@gmail.com>
2021-05-10 20:49:43 +0200

    more things

--- hello/views.py
+++ hello/views.py
@@ -29,20 +29,61 @@ def about(request):
 
 def companies(request):
     companies = Company.objects.all()
+
+    results = []
+    for company in companies:
+        results.append(evaluateCompany(company))
+
     logger.warning(companies)
-    context= {'companies': companies}
+    context= {'results': results}
     return render(request, "companies.html", context)
+
+def evaluateCompany(company):
+#
+# Hier könnten die Auswertung der Daten gemacht werden. Vielleicht ist es aber besser diese direkt beim hochladen zu machen und ins company objekt zu speichern, damit nicht jedes Mal alle Routen analysiert werden müssen.
+#
+    totalemissions = {         
+        'flug': 0,
+        'bahn': 0,
+        'lkw' : 0,
+        'sonstige' : 0
+    }
+# Auswertung der Routen
+    for route in company.routes.all():
+        totalemissions['flug'] += route.distance_plane * route.quantity * 20
+
+    
+    zulieferung = 4
+    intern = 3
+    zustellung = 1
+
+
+    context = {
+        'name': company.name,
+        'id': company.id,
+        'rating': 'silver',
+        'zulieferung_yellow': range(zulieferung),
+        'zulieferung_gray': range(5-zulieferung),
+        'intern_yellow': range(intern),
+        'intern_gray': range(5-intern),
+        'zustellung_yellow': range(zustellung),
+        'zustellung_gray': range(5-zustellung),
+        'emissions': {
+            'flug': 20,
+            'bahn': 40,
+            'lkw' : 30,
+            'sonstige' : 10
+        }
+    }
+    return context
 
 def company(request, pk):
 
     company = Company.objects.get(pk=pk)
 
+
+    context = evaluateCompany(company)
     
-    context = {
-
-        'company': company
-
-    }
 
     return render(request, 'company.html', context)
 
