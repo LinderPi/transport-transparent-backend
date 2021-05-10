363bfe82fe2b327bbdf2a97987b41d446d59a340
Pascal Linder <pascal.linder@sbb.ch>
2021-05-10 16:24:49 +0200

    Read uploaded CSV file

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from api.models import Company, CsvFile
 from .models import Greeting
 
 # Create your views here.
@@ -9,7 +10,14 @@ def index(request):
     return render(request, "index.html")
 
 def calculator(request):
-    return render(request, "calculator.html")
+    if request.method == 'GET':
+        return render(request, "calculator.html")
+
+    csv_file = request.FILES['file']
+    print(csv_file.read().decode('UTF-8'))
+
+    return render(request, "calculator.html") # TODO: go to results
+
 def about(request):
     return render(request, "about.html")
 
