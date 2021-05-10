32960cf1a39c81f0c05f593c56780a8c4abd1847
Pascal Linder <pascal.linder@sbb.ch>
2021-05-10 14:04:20 +0200

    Add new company fields

--- api/models.py
+++ api/models.py
@@ -2,8 +2,15 @@ from django.db import models
 
 class Company(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    tonnage = models.PositiveIntegerField()
-    distance = models.PositiveIntegerField()
+    tonnage = models.PositiveIntegerField(default=0)
+    distance_train = models.PositiveIntegerField(default=0)
+    distance_truck = models.PositiveIntegerField(default=0)
+    distance_ship = models.PositiveIntegerField(default=0)
+    distance_plane = models.PositiveIntegerField(default=0)
+    distance_bike = models.PositiveIntegerField(default=0)
+    distance_others = models.PositiveIntegerField(default=0)
+    transports_per_day = models.PositiveIntegerField(default=0)
+    short_term = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
--- api/serializers.py
+++ api/serializers.py
@@ -4,7 +4,8 @@ from .models import Company, CsvFile
 class CompanySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Company
-        fields = ['id', 'name', 'tonnage', 'distance']
+        fields = ['id', 'name', 'tonnage', 'distance_train', 'distance_truck', 'distance_ship', 'distance_plane',
+                  'distance_bike', 'distance_others', 'transports_per_day', 'short_term']
 
 class CsvFileSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
